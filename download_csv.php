0315cfa66a7f0e12541af1bb8b5d35d43f524f1c
Antti Kurittu <antti.kurittu@gmail.com>
2016-08-22 14:35:51 +0300

    Fixed the CSV downloader to please Excel.

--- download_csv.php
+++ download_csv.php
@@ -1,7 +1,8 @@
 <?php
 $case_number = preg_replace("/[^0-9]/", "", (substr($_GET['case'], 0, 5)));
 header('Content-Description: File Transfer');
-header('Content-Type: application/octet-stream');
+header('Content-Encoding: UTF-8');
+header('Content-Type: text/html; charset=utf-8');
 header('Content-Disposition: attachment; filename=Kirjuri_request_' . $case_number . '.csv');
 require_once("./include_functions.php");
 $kirjuri_database = db('kirjuri-database');
@@ -10,8 +11,10 @@ $query->execute(array(
   ':id' => $case_number
 ));
 $query_results = $query->fetchAll(PDO::FETCH_ASSOC);
+echo "sep=;";
+echo "\n";
 foreach (array_keys($query_results[0]) as $key) {
-  echo $key . ",";
+  echo $key . ";";
 }
 echo "\n";
 foreach ($query_results as $result) {
@@ -19,7 +22,8 @@ foreach ($query_results as $result) {
     $item = str_replace("'", "\"", $result);
     $item = str_replace("\"", "\"\"", $item);
     $item = str_replace("\\", "", $item);
-    echo '"' . $item . '",';
+    $item = str_replace(";", ",", $item);
+    echo '"' . $item . '";';
   };
   echo "\n";
 };

490a5653ff86f05995b890c119d7fb234def127d
Antti Kurittu <antti.kurittu@gmail.com>
2016-08-23 11:50:25 +0300

    Made a jumplist to easily move from device to another.

--- device_memo.php
+++ device_memo.php
@@ -1,11 +1,13 @@
 <?php
 require_once("./include_functions.php");
 $kirjuri_database = db('kirjuri-database');
+
 $query = $kirjuri_database->prepare('select * FROM exam_requests WHERE id = :db_row LIMIT 1');
 $query->execute(array(
     ':db_row' => $_GET['db_row']
 ));
 $mediarow = $query->fetchAll(PDO::FETCH_ASSOC);
+
 $query = $kirjuri_database->prepare('select id, device_type, device_manuf, device_model, device_host_id FROM exam_requests WHERE device_host_id = :db_row');
 $query->execute(array(
     ':db_row' => $_GET['db_row']
@@ -25,6 +27,14 @@ $query->execute(array(
     ':parent_id' => $casefetch['parent_id']
 ));
 $caserow = $query->fetchAll(PDO::FETCH_ASSOC);
+
+$query = $kirjuri_database->prepare('select id, parent_id, device_type, device_manuf, device_model FROM exam_requests WHERE parent_id = :parent_id AND id != :parent_id');
+$query->execute(array(
+    ':parent_id' => $casefetch['parent_id']
+));
+$case_device_id_list = $query->fetchAll(PDO::FETCH_ASSOC);
+
+
 $query = $kirjuri_database->prepare('select id, case_id, case_name, case_suspect, case_added_date FROM exam_requests WHERE case_status <= 2 AND parent_id = id AND is_removed = "0" ORDER BY id ASC');
 $query->execute();
 $allcases = $query->fetchAll(PDO::FETCH_ASSOC);
@@ -32,6 +42,7 @@ echo $twig->render('device_memo.html', array(
     'device_actions' => $_SESSION['lang']['device_actions'],
     'device_locations' => $_SESSION['lang']['device_locations'],
     'connectedmediarow' => $connectedmediarow,
+    'case_device_id_list' => $case_device_id_list,
     'hostdevice' => $hostdevice,
     'mediarow' => $mediarow,
     'allcases' => $allcases,

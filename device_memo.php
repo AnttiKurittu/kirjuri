cdd834bb508db9d4975901382e80204c68c0168c
Antti Kurittu <antti.kurittu@gmail.com>
2016-08-25 11:27:23 +0300

    Fixed a bug where device jump list would show removed devices.

--- device_memo.php
+++ device_memo.php
@@ -2,18 +2,18 @@
 require_once("./include_functions.php");
 $kirjuri_database = db('kirjuri-database');
 
-$query = $kirjuri_database->prepare('select * FROM exam_requests WHERE id = :db_row LIMIT 1');
+$query = $kirjuri_database->prepare('select * FROM exam_requests WHERE is_removed != "1" AND id = :db_row LIMIT 1');
 $query->execute(array(
     ':db_row' => $_GET['db_row']
 ));
 $mediarow = $query->fetchAll(PDO::FETCH_ASSOC);
 
-$query = $kirjuri_database->prepare('select id, device_type, device_manuf, device_model, device_host_id FROM exam_requests WHERE device_host_id = :db_row');
+$query = $kirjuri_database->prepare('select id, device_type, device_manuf, device_model, device_host_id FROM exam_requests WHERE is_removed != "1" AND device_host_id = :db_row');
 $query->execute(array(
     ':db_row' => $_GET['db_row']
 ));
 $connectedmediarow = $query->fetchAll(PDO::FETCH_ASSOC);
-$query = $kirjuri_database->prepare('select id, device_type, device_manuf, device_model, device_host_id FROM exam_requests WHERE id = :db_row');
+$query = $kirjuri_database->prepare('select id, device_type, device_manuf, device_model, device_host_id FROM exam_requests WHERE is_removed != "1" AND id = :db_row');
 $query->execute(array(
     ':db_row' => $mediarow[0]['device_host_id']
 ));
@@ -22,13 +22,13 @@ foreach ($mediarow as $entry)
   {
     $casefetch = $entry;
   }
-$query = $kirjuri_database->prepare('select * FROM exam_requests WHERE id = :parent_id LIMIT 1');
+$query = $kirjuri_database->prepare('select * FROM exam_requests WHERE is_removed != "1" AND id = :parent_id LIMIT 1');
 $query->execute(array(
     ':parent_id' => $casefetch['parent_id']
 ));
 $caserow = $query->fetchAll(PDO::FETCH_ASSOC);
 
-$query = $kirjuri_database->prepare('select id, parent_id, device_type, device_manuf, device_model FROM exam_requests WHERE parent_id = :parent_id AND id != :parent_id');
+$query = $kirjuri_database->prepare('select id, parent_id, device_type, device_manuf, device_model FROM exam_requests WHERE is_removed != "1" AND parent_id = :parent_id AND id != :parent_id');
 $query->execute(array(
     ':parent_id' => $casefetch['parent_id']
 ));

81987d8e4754c7dd16c93ad7727635ab24a7ac6b
Antti Kurittu <antti.kurittu@gmail.com>
2016-08-26 14:14:22 +0300

    Allowed multiple file uploads.

--- upload.php
+++ upload.php
@@ -14,32 +14,47 @@ if (!file_exists($target_dir)) {
   };
 };
 
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$total = count($_FILES['fileToUpload']['name']);
 
-if (file_exists($target_file)) {
-    header('Location: ' . preg_replace('/\?.*/', '', $_SERVER['HTTP_REFERER'])."?case=".substr($_GET['case'], 0, 5)."&upload_status=file_exists");
-    die;
+$uploadstatus = "ok";
+
+  for($i=0; $i<$total; $i++) {
+
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"][$i]);
+    if (file_exists($target_file)) {
+        $uploadstatus = "file_exists";
+        $skip = True;
+        continue;
+    };
+
+    if ($_FILES["fileToUpload"]["size"][$i] > $settings['max_attachment_size']) {
+      $uploadstatus = "filesize_too_large";
+      $skip = True;
+      continue;
+    };
+
+    if (strpos(mime_content_type($_FILES["fileToUpload"]["tmp_name"][$i]), "text") !== false) {
+      if (
+         (substr(strtolower($target_file), -4) !== ".txt") ||
+         (substr(strtolower($target_file), -4) !== ".csv")
+         ) {
+      $target_file = $target_file.".txt";
+      }
+    };
+
+    if ((move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file)) && ($skip !== True)) {
+        $upload_error = False;
+      } else {
+        $upload_error = True;
+        }
+}
+
+if ($upload_error === False) {
+header('Location: ' . preg_replace('/\?.*/', '', $_SERVER['HTTP_REFERER'])."?case=".substr($_GET['case'], 0, 5)."&upload_status=".$uploadstatus);
+die;
+} else {
+header('Location: ' . preg_replace('/\?.*/', '', $_SERVER['HTTP_REFERER'])."?case=".substr($_GET['case'], 0, 5)."&upload_status=error");
+die;
 };
 
-if ($_FILES["fileToUpload"]["size"] > $settings['max_attachment_size']) {
-    header('Location: ' . preg_replace('/\?.*/', '', $_SERVER['HTTP_REFERER'])."?case=".substr($_GET['case'], 0, 5)."&upload_status=filesize_too_large");
-    die;
-};
-
-if (strpos(mime_content_type($_FILES["fileToUpload"]["tmp_name"]), "text") !== false) {
-  if (
-     (substr(strtolower($target_file), -4) !== ".txt") ||
-     (substr(strtolower($target_file), -4) !== ".csv")
-     ) {
-  $target_file = $target_file.".txt";
-  }
-};
-
-if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-      header('Location: ' . preg_replace('/\?.*/', '', $_SERVER['HTTP_REFERER'])."?case=".substr($_GET['case'], 0, 5)."&upload_status=ok");
-      die;
-  } else {
-      header('Location: ' . preg_replace('/\?.*/', '', $_SERVER['HTTP_REFERER'])."?case=".substr($_GET['case'], 0, 5)."&upload_status=error");
-      die;
-    }
 ?>

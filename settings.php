90aba4658c22d4c853caf3d43fe5c8cf9b1474c4
Antti Kurittu <antti.kurittu@gmail.com>
2016-08-31 16:14:40 +0300

    Added a warning if default settings has new directives that are missing from the local configuration read from conf/settings.local or /etc/kirjuri/settings.local.

--- settings.php
+++ settings.php
@@ -8,6 +8,9 @@ if (($save_target === "settings") && (isset($_POST['settings_conf'])))
     if(file_exists($settings_file)) {
       file_put_contents($settings_file, $_POST['settings_conf']);
       logline("Admin", "Settings saved.");
+      $settings_contents = parse_ini_file($settings_file, true);
+      $_SESSION['lang'] = parse_ini_file("conf/" . $settings_contents['settings']['lang'], true);
+      $settings = $settings_contents['settings'];
     } else {
       trigger_error("Settings file ".$settings_file." not found.");
     }
@@ -30,6 +33,12 @@ if ($settings['show_log'] === "1") {
 };
 
 $settings_filedump = file_get_contents($settings_file);
+
+$default_settings = parse_ini_file('conf/settings.conf', true);
+$diff = array_diff_key($default_settings['settings'], $settings_contents['settings']);
+foreach($diff as $key => $value) {
+  trigger_error('Missing a settings directive from '.$settings_file.': '.$key.' = "'.$value.'". The default configuration file might have changed on update, please update your local settings file to contain this directive under [settings].');
+}
 
 echo $twig->render('settings.html', array(
     'settings_filedump' => $settings_filedump,

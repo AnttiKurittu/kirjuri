037dd84af4037ad4c4f2371ff0d0913657aede69
Antti Kurittu <antti.kurittu@gmail.com>
2016-12-24 23:33:16 +0200

    Bugfixes.

--- case_report.php
+++ case_report.php
@@ -2,16 +2,18 @@
 
 require_once './include_functions.php';
 protect_page(2); // View only or higher
+$id = num($_GET['case']);
+verify_owner($id);
 
 
 $query = $kirjuri_database->prepare('SELECT * FROM exam_requests WHERE id=:id AND parent_id=:id LIMIT 1');
 $query->execute(array(
-    ':id' => $_GET['case'],
+    ':id' => $id,
 ));
 $caserow = $query->fetchAll(PDO::FETCH_ASSOC);
 $query = $kirjuri_database->prepare('SELECT * FROM exam_requests WHERE id != :id AND parent_id=:id ORDER BY device_type');
 $query->execute(array(
-    ':id' => $_GET['case'],
+    ':id' => $id,
 ));
 $mediarow = $query->fetchAll(PDO::FETCH_ASSOC);
 echo $twig->render('case_report.html', array(
